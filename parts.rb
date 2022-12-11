5b2d896776f189d18fdea7dd49fefe1d3ea91a98
Aleix Morgadas <hello@aleixmorgadas.dev>
2022-12-11 17:08:05 +0000

    improving parts

--- parts.rb
+++ parts.rb
@@ -3,7 +3,8 @@ module MadLibs
     nouns:        %w(competitive\ advantage learning\ organization
                    big\ data growth value digital\ business
                    platform revolution culture ecosystem internet\ of\ things 
-                   insight\ from\ data virtual\ reality artificial\ intelligence blockchain design\ thinking digital\ transformation),
+                   insight\ from\ data virtual\ reality artificial\ intelligence blockchain design\ thinking digital\ transformation
+                   serverless),
     plural_nouns: %w(social\ media leaders data\ leaders),
     adjectives:   %w(digital\ first networked collaborative cloud\ based secure 
                    customer\ focused disruptive sustainable agile open 
